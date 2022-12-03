1916e9d7aeba94927911dc9bb9b06eeddc9cf5e3
Benjamin Tucker <benjamin.tucker@hey.com>
2022-12-02 16:30:51 -0800

    day 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
 fn main() {
-    println!("Hello, world!");
+    let mut sums: Vec<i32> = read_elves().iter().map(|elf| elf.iter().sum()).collect();
+    sums.sort_unstable();
+    sums.reverse();
+    let total: i32 = sums.iter().take(3).sum();
+    println!("{}", total);
+}
+
+fn read_elves() -> Vec<Vec<i32>> {
+    let mut result = Vec::new();
+    let mut current = Vec::new();
+    for line_ in std::io::stdin().lines() {
+        let line = line_.unwrap();
+        if let Ok(num) = line.parse::<i32>() {
+            current.push(num);
+        } else if !current.is_empty() {
+            result.push(current);
+            current = Vec::new();
+        }
+    }
+    if !current.is_empty() {
+        result.push(current);
+    }
+    result
 }
