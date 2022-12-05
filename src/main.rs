5845a04fedfd59e9cfd1be9af9a52bf66eebafb8
Benjamin Tucker <benjamin.tucker@hey.com>
2022-12-04 17:56:46 -0800

    day 3

--- src/main.rs
+++ src/main.rs
@@ -1,87 +1,31 @@
+use std::collections::HashSet;
+
 fn main() {
     let mut score = 0;
-    for ln in std::io::stdin().lines().map(|l| l.unwrap()) {
-        let (opp, you) = parse_rps_line(&ln);
-        score += you.shape_score() + you.vs(&opp).outcome_score();
+    let lines: Vec<String> = std::io::stdin().lines().map(|r| r.unwrap()).collect();
+    for i in (0..lines.len()).step_by(3) {
+        let a = parse_rucksack(&lines[i]);
+        let b = parse_rucksack(&lines[i + 1]);
+        let c = parse_rucksack(&lines[i + 2]);
+        score += item_priority(find_badge(&a, &b, &c).unwrap());
     }
-    println!("{}", score);
+    println!("Ans: {}", score);
 }
 
-enum RPS {
-    Rock,
-    Paper,
-    Scissors,
+fn parse_rucksack(ln: &str) -> HashSet<u8> {
+    ln.as_bytes().iter().copied().collect()
 }
 
-#[derive(PartialEq)]
-enum Outcome {
-    Beats,
-    Ties,
-    Loses,
-}
-
-fn parse_rps_line(ln: &str) -> (RPS, RPS) {
-    let pair: Vec<&str> = ln.split_whitespace().collect();
-    let opponent = match pair[0] {
-        "A" => RPS::Rock,
-        "B" => RPS::Paper,
-        "C" => RPS::Scissors,
-        _ => panic!(),
-    };
-    let outcome = match pair[1] {
-        "X" => Outcome::Loses,
-        "Y" => Outcome::Ties,
-        "Z" => Outcome::Beats,
-        _ => panic!(),
-    };
-    let you = rps_for_outcome(&outcome, &opponent);
-    (opponent, you)
-}
-
-fn rps_for_outcome(outcome: &Outcome, vs: &RPS) -> RPS {
-    let result = match (outcome, vs) {
-        (Outcome::Beats, RPS::Scissors)
-        | (Outcome::Ties, RPS::Rock)
-        | (Outcome::Loses, RPS::Paper) => RPS::Rock,
-        (Outcome::Beats, RPS::Rock)
-        | (Outcome::Ties, RPS::Paper)
-        | (Outcome::Loses, RPS::Scissors) => RPS::Paper,
-        (Outcome::Beats, RPS::Paper)
-        | (Outcome::Ties, RPS::Scissors)
-        | (Outcome::Loses, RPS::Rock) => RPS::Scissors,
-    };
-    assert!(result.vs(vs) == *outcome);
-    result
-}
-
-impl RPS {
-    fn vs(&self, opp: &RPS) -> Outcome {
-        match (self, opp) {
-            (RPS::Rock, RPS::Scissors) | (RPS::Paper, RPS::Rock) | (RPS::Scissors, RPS::Paper) => {
-                Outcome::Beats
-            }
-            (RPS::Rock, RPS::Rock) | (RPS::Paper, RPS::Paper) | (RPS::Scissors, RPS::Scissors) => {
-                Outcome::Ties
-            }
-            _ => Outcome::Loses,
-        }
-    }
-
-    fn shape_score(&self) -> i32 {
-        match self {
-            RPS::Rock => 1,
-            RPS::Paper => 2,
-            RPS::Scissors => 3,
-        }
+fn item_priority(item: u8) -> i32 {
+    if b'A' <= item && item <= b'Z' {
+        return (item as i32) - (b'A' as i32) + 27;
+    } else if b'a' <= item && item <= b'z' {
+        return (item as i32) - (b'a' as i32) + 1;
+    } else {
+        panic!();
     }
 }
 
-impl Outcome {
-    fn outcome_score(self) -> i32 {
-        match self {
-            Outcome::Beats => 6,
-            Outcome::Ties => 3,
-            Outcome::Loses => 0,
-        }
-    }
+fn find_badge(a: &HashSet<u8>, b: &HashSet<u8>, c: &HashSet<u8>) -> Option<u8> {
+    a.intersection(b).find(|item| c.contains(item)).copied()
 }
